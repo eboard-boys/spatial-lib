1e8da76b0f36761241dd50c8d4839dfda60c7df1
Carson Bush <hyperdriveguy@gmail.com>
2023-11-04 16:16:17 -0600

    Make scalar and vector versions of Euler's

--- src/vectors.c
+++ src/vectors.c
@@ -4,15 +4,18 @@
 #include <math.h>
 
 // Get the magnitude of a given vector
-vector_element_t magnitude(vector_t vec) {
+magnitude_t magnitude(vector_t vec) {
     return root_func((vec[x] * vec[x]) + (vec[x] * vec[x]) + (vec[x] * vec[x]));
 }
 
 // Eulers method for integration in respect to time in 3 dimensions.
 // Example: pass acceleration vector to source and velocity to target to integrate acceleration
-void euler_step(vector_t source_vec, vector_t target_vec, float delta_t) {
+void vector_euler_step(vector_t source_vec, vector_t target_vec, float delta_t) {
     target_vec[x] += source_vec[x] * delta_t;
     target_vec[y] += source_vec[y] * delta_t;
     target_vec[z] += source_vec[z] * delta_t;
 }
 
+void euler_step(magnitude_t source, magnitude_t* destination, float delta_t) {
+    *destination += source * delta_t;
+}
--- src/vectors.h
+++ src/vectors.h
@@ -2,6 +2,8 @@
 #define vector_element_t float
 #define vector_t vector_element_t*
 #define root_func sqrtf
+// type used for vector magnitude
+#define magnitude_t vector_element_t
 
 // elements are assigned indexes in the backing array
 enum direction {x, y, z};
