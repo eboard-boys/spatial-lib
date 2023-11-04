4a4cc8ac0a1c6a31bac93417102cef452654214e
Carson Bush <hyperdriveguy@gmail.com>
2023-11-04 15:08:23 -0600

    Add Euler's method

--- src/vectors.c
+++ src/vectors.c
@@ -3,7 +3,14 @@
 #include "vectors.h"
 #include <math.h>
 
+// Get the magnitude of a given vector
 vector_element_t magnitude(vector_t vec) {
     return root_func((vec[x] * vec[x]) + (vec[x] * vec[x]) + (vec[x] * vec[x]));
 }
 
+// Euler's method for integration in respect to time in 3 dimensions
+void update_velocity(vector_t accel_vec, vector_t velo_vec, float delta_t) {
+    velo_vec[x] += accel_vec[x] * delta_t;
+    velo_vec[y] += accel_vec[y] * delta_t;
+    velo_vec[z] += accel_vec[z] * delta_t;
+}
