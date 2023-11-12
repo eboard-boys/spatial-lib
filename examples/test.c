d965d195b21d97a974cd7d63894170c0d09ad1b3
HansenDJ <hansendj38@gmail.com>
2023-11-12 01:01:47 -0700

    Delete time_t and implement clock_t for delta_t

--- examples/test.c
+++ examples/test.c
@@ -4,65 +4,59 @@
 #include "../src/accel_dummy.h"
 #include "../src/vectors.h"
 
-// Index values for positions of values within time_array
-#define i_prev_time 0
-#define i_curr_time 1
-
 int main(void) {
-    // Start timer - time in seconds
-    time_t t = time(NULL);
-    // Array that stores time at previous clock cycle and current time
-    time_t time_array[2] = {0,0};
+    clock_t prev_time, curr_time;
+    double delta_t;
+
+    prev_time = clock();
+    curr_time = clock();
 
     init_accelerometer();
 
     // Initialize values
-    // float t = 0;
     acc_vec_t raw_acceleration;
     vector_t acceleration[3] = {0,0,0};
     vector_t velocity[3] = {0,0,0};
     float speed = 0;
     vector_t displacement[3] = {0,0,0};
     float arc_length = 0;
-
-    // Append current elapsed time in seconds to array each cycle
-    time_array[i_curr_time] = t;
-    float delta_t = (float)time_array[i_curr_time] - time_array[i_prev_time];
     
-    raw_acceleration = get_current_acceleration();
-    acceleration[x] = raw_acceleration.x;
-    acceleration[y] = raw_acceleration.y;
-    acceleration[z] = raw_acceleration.z;
+    // Type ctrl + c to end program
+    while(1) {
+        curr_time = clock();
+        printf("\n----Starting of data, prev_time = %ld cycles----", prev_time);
+        delta_t = (double)(curr_time - prev_time) / CLOCKS_PER_SEC; // Number seconds since last clock cycle recorded
+        
+        raw_acceleration = get_current_acceleration();
+        acceleration[x] = raw_acceleration.x;
+        acceleration[y] = raw_acceleration.y;
+        acceleration[z] = raw_acceleration.z;
 
-    // Calculate and display acceleration
-    printf("\nAcceleration: %f %f %f\n", acceleration[x], acceleration[y], acceleration[z]);
+        // Calculate and display acceleration
+        printf("\nAcceleration: <%f, %f, %f>\n", acceleration[x], acceleration[y], acceleration[z]);
 
-    // Print the time delta that is used for Euler's
-    printf("dt: %f\n", delta_t);
+        // Print the time delta that is used for Euler's
+        printf("dt: %f\n", delta_t);
 
-    // Calculate and display velocity
-    vector_euler_step(acceleration, velocity, delta_t);
-    printf("Velocity: %f %f %f\n", velocity[x], velocity[y], velocity[z]);
+        // Calculate and display velocity
+        vector_euler_step(acceleration, velocity, delta_t);
+        printf("Velocity: <%f, %f, %f>\n", velocity[x], velocity[y], velocity[z]);
 
-    // Calculate and display speed (magnitude of velocity)
-    speed = magnitude(velocity);
-    printf("Speed: %f\n", speed);
+        // Calculate and display speed (magnitude of velocity)
+        speed = magnitude(velocity);
+        printf("Speed: %f\n", speed);
 
-    // Calculate and display displacement
-    vector_euler_step(velocity, displacement, delta_t);
-    printf("Displacement: %f %f %f\n", displacement[x], displacement[y], displacement[z]);
+        // Calculate and display displacement
+        vector_euler_step(velocity, displacement, delta_t);
+        printf("Displacement: <%f, %f, %f>\n", displacement[x], displacement[y], displacement[z]);
 
-    // What are you trying to do here???
-    // Calculate and display distance
-    // euler_step(displacement, distance, delta_t);
-    // printf("Distance: %f\n", distance);
-
-    // Calculate and display distance/arc length (odometer) - (integrate speed in respect to time)
-    euler_step(magnitude(velocity), &arc_length, delta_t);
-    printf("Arc Length: %f\n", arc_length);
-
-    // Assign current time to previous time before start of new clock cycle
-    time_array[i_prev_time] = t;
+        // Calculate and display distance/arc length (odometer) - (integrate speed in respect to time)
+        euler_step(magnitude(velocity), &arc_length, delta_t);
+        printf("Arc Length: %f\n", arc_length);
+        
+        prev_time = curr_time;
+        printf("End of data, curr_time = %ld\n", curr_time);
+    }
 
     return 0;
 }
