8d6d006d09951b4dd2630cc3306cb29b2eef845e
HansenDJ <hansendj38@gmail.com>
2023-11-18 22:28:13 -0700

    Delete hardware driver, add plot library

--- examples/adaptive_throttle_test.c
+++ examples/adaptive_throttle_test.c
@@ -3,4 +3,4 @@
 #include "../src/vectors.h"
 
 float throttle_array[6] = {0, 2.5, 3, 3.2, 4.8, 3.5}; // Fake throttle values
-
+vector_t acceleration_array[6] = {1.25, 0.25, 0.1, 0.8, 0.8, -0.65}; // Fake throttle values
