37a50dcedf58304c1dbe4175e1aed4a1cc13a721
HansenDJ <hansendj38@gmail.com>
2023-11-18 22:44:34 -0700

    remove interface

--- src/interface.c
+++
@@ -1,5 +0,0 @@
-/* High level interface function for updating and fetching sensor values.
- * Licensed under the LGPL. */
-#include "../include/accel_interface.h"
-#include "accel_mpu6050.h"
-#include "vectors.h"
