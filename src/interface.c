16471385bfbd3299da952f08d954cc65da5efeab
Carson Bush <hyperdriveguy@gmail.com>
2023-11-25 17:03:21 -0700

    Purge old driver from the face of the earth I will extract the useful parts and make it so none of us half to sift through a 10000+ line source file

--- src/interface.c
+++ src/interface.c
@@ -1,18 +1,5 @@
 /* High level interface function for updating and fetching sensor values.
  * Licensed under the LGPL. */
-#include "../include/accel_interface.h"
-//#include "accel_mpu6050.h"
-//#include "vectors.h"
-
-uint8_t init_accelerometer(I2C_HandleTypeDef* i2c_handler) {
-	if (set_iic_handler(i2c_handler))
-		return 1;
-	if (mpu6050_basic_init())
-		return 1;
-	return 0;
-}
-
-uint8_t get_acceleration(float acceleration[3]) {
-	float gyro[3];
-	return mpu6050_basic_read(acceleration, gyro);
-}
+#include "../include/interface.h"
+#include "accel_mpu6050.h"
+#include "vectors.h"
